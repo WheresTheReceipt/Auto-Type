e6613efe75ddad899be312d057d8b41e322e5a6e
WheresTheReceipt <55933527+WheresTheReceipt@users.noreply.github.com>
2019-09-28 21:18:38 -0400

    Update type.py

--- type.py
+++ type.py
@@ -2,6 +2,7 @@ import pyautogui
 import time
 import random
 
+#This makes sure the user enters a Number
 try:
     enter = int(input('Enter the amount of time you want the bot to type the command: '))
 except ValueError:
@@ -13,9 +14,13 @@ print('Starting Program you have 5 seconds.')
 time.sleep(5)
 Entercommand = random.randint(15, 26)
 while times < enter:
-    pyautogui.typewrite('.work')
-    pyautogui.press('enter')
+    pyautogui.typewrite('CHANGE ME')     #This is where you type the message you want
+    pyautogui.press('enter')       #This automatically clicks your enter key
     times += 1
-    time.sleep(random.randint(15, 47))
+    
+    #You have 2 option here based on your circumstances. Uncomment Only One of them.
+    
+    #time.sleep(random.randint(15, 47))    #You can change these if you want. This is the random time it will take to send the command.
+    #time.sleep(1)               #This line if you want the bot to send the message every second.
 
-print(times)
+    print(times)                #Prints how many times the bot typed in the Word/Command
